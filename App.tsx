ab51e3a70c3640d9087bac2806383282649b8260
Sam <sam.sahbaei.razavi@iths.se>
2023-11-26 21:36:29 +0100

    lektion 5- nästan klar

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';;
 import { Button } from '@rneui/base';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import UserList from './src/screens/UserList/UserList';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { store } from './src/store/store';
 import { UserForm } from './src/screens/UserForm/UserForm';
 import { ToastProvider } from 'react-native-toast-notifications'
@@ -14,37 +14,38 @@ import { UserInfo } from './src/screens/UserInfo/UserInfo';
 const UserListStack = createNativeStackNavigator()
 
 const UserListStackScreen = () => {
-    return (
-        <UserListStack.Navigator>
-            <UserListStack.Screen name='UserList' component={UserList} />
-            <UserListStack.Screen name='UserInfo' component={UserInfo} />
-        </UserListStack.Navigator>
-    )
+  return (
+    <UserListStack.Navigator>
+      <UserListStack.Screen name="UserList" component={UserList} />
+      <UserListStack.Screen name="UserInfo" component={UserInfo} />
+    </UserListStack.Navigator>
+  )
 }
 
+const Tab = createBottomTabNavigator();
 
-  const Tab = createBottomTabNavigator();;
+const NavigationWrapper = () => {
+  const loggedInAs = useSelector((state: any) => state.auth.loggedInAs)
+
+  return (
+    <NavigationContainer>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="UserListStack" component={UserListStackScreen} />
+          <Tab.Screen name="UserForm" component={UserForm} options={{ headerShown: true }} />
+          {loggedInAs && (
+            <Tab.Screen name="UserInfo" component={UserInfo} options={{ title: `${loggedInAs.firstName} ${loggedInAs.lastName}`}} />
+          )}
+        </Tab.Navigator>
+      </NavigationContainer>
+  )
+}
 
 export default function App() {
   return (
     <ToastProvider>
-    <Provider store={store}>
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="UserList" component={UserListStackScreen} />
-        <Tab.Screen name="UserForm" component={UserForm}  />
-      </Tab.Navigator>
-    </NavigationContainer>
-    </Provider>
+      <Provider store={store}>
+        <NavigationWrapper />
+      </Provider>
     </ToastProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  loggedInAs: undefined,
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logIn: (state, action) => {
+        console.log('called logIn action: ', action);
+      state.loggedInAs = action.payload
+    },
+    logOut: (state) => {
+      state.loggedInAs = undefined
+    },
+  },
+})
+
+export const { logIn, logOut } = authSlice.actions
+
+export default authSlice.reducer
